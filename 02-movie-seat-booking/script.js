05f959cad7d3e873ec0364fa77824288902a48e0
Jeniffer Callou <jenifferprc@gmail.com>
2022-03-19 21:22:52 +0100

    add set movie data functon;

--- 02-movie-seat-booking/script.js
+++ 02-movie-seat-booking/script.js
@@ -4,6 +4,24 @@ const count = document.getElementById('count');
 const total = document.getElementById('.total');
 const movieSelect = document.querySelector('.movie');
 
+populateUI();
+
+// Update total and count
+function updateSelectedCount() {
+    const selectedSeats = document.querySelectorAll('.row .seat.selected');
+  
+    const seatsIndex = [...selectedSeats].map(seat => [...seats].indexOf(seat));
+  
+    localStorage.setItem('selectedSeats', JSON.stringify(seatsIndex));
+  
+    const selectedSeatsCount = selectedSeats.length;
+  
+    count.innerText = selectedSeatsCount;
+    total.innerText = selectedSeatsCount * ticketPrice;
+    
+    setMovieData(movieSelect.selectedIndex, movieSelect.value);
+}
+
 // Get data from localStorage and populate UI
 function populatetUI() {
     const selectedSeats = JSON.parse(localStorage.getItem('selectedSeats'));
@@ -41,3 +59,6 @@ container.addEventListener('click', e => {
       updateSelectedCount();
     }
 });
+
+// Initial count and total set
+updateSelectedCount();
