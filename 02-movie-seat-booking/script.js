3c209cef1fdf4b290cc0b6f411da2cb82455f32b
Jeniffer Callou <jenifferprc@gmail.com>
2022-03-19 20:21:44 +0100

    add selectedSeats in populateUI function into js

--- 02-movie-seat-booking/script.js
+++ 02-movie-seat-booking/script.js
@@ -3,3 +3,16 @@ const seats = document.querySelectorAll('.row .seat:not(.occupied)');
 const count = document.getElementById('count');
 const total = document.getElementById('.total');
 const movieSelect = document.querySelector('.movie');
+
+// Get data from localStorage and populate UI
+function populatetUI() {
+    const selectedSeats = JSON.parse(localStorage.getItem('selectedSeats'));
+
+    if (selectedSeats !== null && selectedSeats.length > 0) {
+        seats.forEach((seat, index) => {
+            if (selectedSeats.indexOf(index) > -1) {
+                seat.classList.add('selected');
+            }
+        });
+    }
+}
