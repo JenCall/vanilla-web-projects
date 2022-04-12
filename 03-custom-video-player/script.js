9803f7e815ea40117c56495fc7b3609c5108f23b
Jeniffer Callou <jenifferprc@gmail.com>
2022-04-12 20:23:30 +0200

    add event listeners

--- 03-custom-video-player/script.js
+++ 03-custom-video-player/script.js
@@ -1,5 +1,14 @@
 const video = document.getElementById('video');
-const play = document.getElementById('play');
-const stop = document.getElementById('stop');
+const playBtn = document.getElementById('play');
+const stopBtn = document.getElementById('stop');
 const progress = document.getElementById('progress');
 const timestamp = document.getElementById('timestamp');
+
+//Event listeners
+video.addEventListener('click', toggleVideoStatus);
+video.addEventListener('pause', updatePlayIcon);
+video.addEventListener('play', updatePlayIcon);
+video.addEventListener('timeupdate', updateProgress);
+playBtn.addEventListener('click', toggleVideoStatus);
+stopBtn.addEventListener('click', stopVideo);
+progress.addEventListener('change', setVideoProgress);
