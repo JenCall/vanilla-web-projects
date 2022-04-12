9bdf63910192929aa003aa5b8f393c0168de622e
Jeniffer Callou <jenifferprc@gmail.com>
2022-04-12 20:26:49 +0200

    add functions of eventListteners

--- 03-custom-video-player/script.js
+++ 03-custom-video-player/script.js
@@ -12,3 +12,28 @@ video.addEventListener('timeupdate', updateProgress);
 playBtn.addEventListener('click', toggleVideoStatus);
 stopBtn.addEventListener('click', stopVideo);
 progress.addEventListener('change', setVideoProgress);
+
+//play & pause video
+function toggleVideoStatus() {
+    return true
+}
+
+//update play/pause icon
+function updatePlayIcon() {
+    return true
+}
+
+//update progress & timestamp
+function updateProgress() {
+    return true
+}
+
+//set video time to progress
+function setVideoProgress() {
+    return true
+}
+
+//stop video
+function stopVideo() {
+    return true
+}
