1fe05c9d56376690393be3710d4d1ad829a0adae
Jeniffer Callou <jenifferprc@gmail.com>
2022-04-15 21:51:51 +0200

    add variables

--- 04-exchange-rate-calculator/script.js
+++ 04-exchange-rate-calculator/script.js
@@ -1,5 +1,13 @@
-function calculate() {
+const currencyElOne = document.getElementById('currency-one');
+const currencyElTwo = document.getElementById('currency-two');
 
-}
+const amountElOne = document.getElementById('amount-one');
+const amountElTwo = document.getElementById('amount-two');
+
+const rateEl = document.getElementById('rate');
+const swap = document.getElementById('swap');
+
+
+function calculate() {}
 
 calculate()
