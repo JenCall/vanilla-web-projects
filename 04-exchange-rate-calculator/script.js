a35aa106dc05f36eae6b731091b5f724bbe54f9c
Jeniffer Callou <jenifferprc@gmail.com>
2022-04-15 21:53:52 +0200

    add event listeners

--- 04-exchange-rate-calculator/script.js
+++ 04-exchange-rate-calculator/script.js
@@ -7,7 +7,13 @@ const amountElTwo = document.getElementById('amount-two');
 const rateEl = document.getElementById('rate');
 const swap = document.getElementById('swap');
 
-
+//Fetch Exchange rates and update the DOM
 function calculate() {}
 
-calculate()
+// Event Listeners
+currencyElOne.addEventListener('change', calculate);
+currencyElTwo.addEventListener('change', calculate);
+amountElOne.addEventListener('input', calculate);
+amountElTwo.addEventListener('input', calculate);
+
+calculate();
