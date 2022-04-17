1c16cacdc7339ca02446cdb5b3da03b504959cef
Jeniffer Callou <jenifferprc@gmail.com>
2022-04-17 21:36:08 +0200

    adjust name

--- 05-DOM-array-methods/script.js
+++ 05-DOM-array-methods/script.js
@@ -12,14 +12,14 @@ async function getRandomUser() {
     const res = await fetch('https://randomuser.me/api');
     const data = await res.json();
 
-    const user = data.result[0];
+    const user = data.results[0];
 
     const newUser = {
         name: `${user.name.first} ${user.name.last}`,
         money: Math.floor(Math.random() * 1000000)
     };
 
-    addData(newUser)
+    addData(newUser);
 }
 
 function addData(obj) {
