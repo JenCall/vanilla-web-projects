a398fc909258922016ff4fd6939c800f76dbd97d
Jeniffer Callou <jenifferprc@gmail.com>
2022-04-17 21:33:10 +0200

    add user + newUser into js

--- 05-DOM-array-methods/script.js
+++ 05-DOM-array-methods/script.js
@@ -11,4 +11,13 @@ let data = [];
 async function getRandomUser() {
     const res = await fetch('https://randomuser.me/api');
     const data = await res.json();
+
+    const user = data.result[0];
+
+    const newUser = {
+        name: `${user.name.first} ${user.name.last}`,
+        money: Math.floor(Math.random() * 1000000)
+    };
+
+    console.log(newUser)
 }
