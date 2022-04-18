099c60558c16f1a5c30ea830dd76e5d528791b2c
Jeniffer Callou <jenifferprc@gmail.com>
2022-04-18 21:34:10 +0200

    add updateDOM function

--- 05-DOM-array-methods/script.js
+++ 05-DOM-array-methods/script.js
@@ -24,4 +24,18 @@ async function getRandomUser() {
 
 function addData(obj) {
     data.push(obj);
+
+    updateDOM();
+}
+
+function updateDOM(providedData = data) {
+    // clear main div
+    main.innerHTML = `<h2><strong>Person</strong>Wealth</h2>`;
+
+    providedData.forEach(item => {
+        const element = document.createElement('div');
+        element.classList.add('person');
+        element.innerHTML = `<strong>${item.name}</strong> ${item.money}`;
+        main.appendChild(element);
+    });
 }
