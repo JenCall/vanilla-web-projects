caaed55b4f3343baa62ef5dc5642e4d0d9660adb
Jeniffer Callou <jenifferprc@gmail.com>
2022-04-28 21:05:41 +0200

    add keydown letter press

--- 07-hangman-game/script.js
+++ 07-hangman-game/script.js
@@ -42,4 +42,31 @@ function displayWord() {
 	}
 }  
 
+// Keydown letter press
+window.addEventListener('keydown', e => {
+	if (playable) {
+		if (e.keyCode >= 65 && e.keyCode <= 90) {
+			const letter = e.key.toLowerCase();
+
+			if (selectedWord.includes(letter)) {
+				if (!correctLetters.includes(letter)) {
+					correctLetters.push(letter);
+
+					displayWord();
+				} else {
+					showNotification();
+				}
+			} else {
+				if (!wrongLetters.includes(letter)) {
+					wrongLetters.push(letter);
+
+					updateWrongLettersEl();
+				} else {
+					showNotification();
+				}
+			}
+		}
+	}
+});
+
 displayWord()
