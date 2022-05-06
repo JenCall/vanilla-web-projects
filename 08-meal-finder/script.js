c536934a165e9c874d18e9bb019962e600adbae2
Jeniffer Callou <jenifferprc@gmail.com>
2022-05-06 21:29:11 +0200

    add searchMeal

--- 08-meal-finder/script.js
+++ 08-meal-finder/script.js
@@ -5,5 +5,17 @@ const search = document.getElementById('search'),
   resultHeading = document.getElementById('result-heading'),
   single_mealEl = document.getElementById('single-meal');
 
+
+// Search meal
+function searchMeal(e) {
+    e.preventDefault();
+  
+    // Clear single meal
+    single_mealEl.innerHTML = '';
+
+    // Get search term
+    const term = search.value;
+}
+
 // Event listeners
 submit.addEventListener('submit', searchMeal);
