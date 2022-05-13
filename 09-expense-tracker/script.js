ef55ba94dd92d1c2eb2c3070620d7d7030dba0fd
Jeniffer Callou <jenifferprc@gmail.com>
2022-05-13 21:07:37 +0200

    adjusted part of addTransaction function

--- 09-expense-tracker/script.js
+++ 09-expense-tracker/script.js
@@ -23,5 +23,7 @@ function addTransaction(transaction) {
     // add class based on value
     item.classList.add(transaction.amount < 0 ? 'minus' : 'plus');
 
-    item.innerHTML = `${transaction.text} <span>${sign}${transaction.amount}</span>`;
+    item.innerHTML = `${transaction.text} <span>${sign}${Math.abs(transaction.amount)}</span> <button class="delete-btn">x</button>`;
+
+    list.appendChild(item);
 }
