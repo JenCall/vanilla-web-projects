4531e35596d222aeb5e93bc11ae536199223e7af
Jeniffer Callou <jenifferprc@gmail.com>
2022-05-19 20:36:17 +0200

    add variables

---
+++ 10-music-player/script.js
@@ -0,0 +1,4 @@
+const musicContainer = document.getElementById('music-container');
+const playBtn = document.getElementById('play');
+const prevBtn = document.getElementById('prev');
+const nextBtn = document.getElementById('next');
