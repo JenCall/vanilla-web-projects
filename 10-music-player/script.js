1c72bfb901f58c24c106a2b6345d012a1752b82a
Jeniffer Callou <jenifferprc@gmail.com>
2022-05-20 20:58:00 +0200

    add prevSong function

--- 10-music-player/script.js
+++ 10-music-player/script.js
@@ -43,3 +43,15 @@ function pauseSong() {
   
     audio.pause();
 }
+
+function prevSong() {
+    songIndex--;
+  
+    if (songIndex < 0) {
+      songIndex = songs.length - 1;
+    }
+  
+    loadSong(songs[songIndex]);
+  
+    playSong();
+}
