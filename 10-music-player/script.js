4e685d4e2148de048cdc6af4ecf9e47abe2fb481
Jeniffer Callou <jenifferprc@gmail.com>
2022-05-22 20:31:28 +0200

    add updateProgress function

--- 10-music-player/script.js
+++ 10-music-player/script.js
@@ -76,6 +76,13 @@ function setProgress(e) {
     audio.currentTime = (clickX / width) * duration;
 }
 
+// Update progress bar
+function updateProgress(e) {
+    const { duration, currentTime } = e.srcElement;
+    const progressPercent = (currentTime / duration) * 100;
+    progress.style.width = `${progressPercent}%`;
+}
+
 //get duration & currentTime for Time of song
 function DurTime (e) {
 	const {duration,currentTime} = e.srcElement;
