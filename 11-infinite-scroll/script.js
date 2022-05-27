25c6b76e6dcd92706910ebf99b004c6e0d985f77
Jeniffer Callou <jenifferprc@gmail.com>
2022-05-27 21:38:09 +0200

    add fetch posts function

--- 11-infinite-scroll/script.js
+++ 11-infinite-scroll/script.js
@@ -4,3 +4,12 @@ const filter = document.getElementById('filter');
 
 let limit = 5;
 let page = 1;
+
+// fetch posts from API
+async function getPosts() {
+    const response = await fetch(`https://jsonplaceholder.typicode.com/posts?_limit=${limit}&_page=${page}`);
+
+    const data = await response.json();
+
+    return data
+}
