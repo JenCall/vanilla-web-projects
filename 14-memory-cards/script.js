51aa9602d879849953189c692eeb1d984163a155
Jeniffer Callou <jenifferprc@gmail.com>
2022-06-22 21:44:42 +0200

    add +variables

--- 14-memory-cards/script.js
+++ 14-memory-cards/script.js
@@ -4,6 +4,11 @@ const nextBtn = document.getElementById('next');
 const currentEl = document.getElementById('current');
 const showBtn = document.getElementById('show');
 const hideBtn = document.getElementById('hide');
+const questionEl = document.getElementById('question');
+const answerEl = document.getElementById('answer');
+const addCardBtn = document.getElementById('add-card');
+const clearBtn = document.getElementById('clear');
+const addContainer = document.getElementById('add-container');
 
 // Store card data
 const cardsData = getCardsData();
