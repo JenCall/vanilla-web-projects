960ca1354211a0f4f3df2008493952bf5cc41d6c
Jeniffer Callou <jenifferprc@gmail.com>
2022-07-06 22:26:00 +0200

    add setTimeout function

--- 16-relaxer-app/script.js
+++ 16-relaxer-app/script.js
@@ -1,9 +1,19 @@
 const container = document.getElementById('container');
 const text = document.getElementById('text');
 
+breathAnimation();
+
 function breathAnimation() {
   text.innerText = 'Breathe In!';
   container.className = 'container grow';
+
+  setTimeout(() => {
+    text.innerText = 'Hold';
+
+    setTimeout(() => {
+      text.innerText = 'Breathe Out!';
+      container.className = 'container shrink';
+    });
+  });
 }
 
-breathAnimation();
