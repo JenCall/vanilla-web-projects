ccc51dc2c32675a3e685520229f8c56afd5f08ad
Jeniffer Callou <jenifferprc@gmail.com>
2022-07-06 22:24:03 +0200

    add breathAnimation function

--- 16-relaxer-app/script.js
+++ 16-relaxer-app/script.js
@@ -1,2 +1,9 @@
 const container = document.getElementById('container');
 const text = document.getElementById('text');
+
+function breathAnimation() {
+  text.innerText = 'Breathe In!';
+  container.className = 'container grow';
+}
+
+breathAnimation();
