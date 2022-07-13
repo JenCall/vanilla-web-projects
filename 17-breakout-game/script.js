e6a1883d863375d6157f3d3d988a69b006c335e2
Jeniffer Callou <jenifferprc@gmail.com>
2022-07-13 22:02:52 +0200

    add drawBall func

--- 17-breakout-game/script.js
+++ 17-breakout-game/script.js
@@ -52,3 +52,12 @@ for (let i = 0; i < brickRowCount; i++) {
     bricks[i][j] = { x, y, ...brickInfo };
   }
 }
+
+// Draw ball on canvas
+function drawBall() {
+    ctx.beginPath();
+    ctx.arc(ball.x, ball.y, ball.size, 0, Math.PI * 2);
+    ctx.fillStyle = ball.visible ? '#0095dd' : 'transparent';
+    ctx.fill();
+    ctx.closePath();
+}
