f6e01b1a20c0af64022b3f573d4554422ed5f23b
Jeniffer Callou <jenifferprc@gmail.com>
2022-07-30 21:35:19 +0200

    add dragStart function

--- 19-sortable-list/script.js
+++ 19-sortable-list/script.js
@@ -44,3 +44,8 @@ function createList() {
             draggable_list.appendChild(listItem);
         })
 }
+
+function dragStart() {
+    // console.log('Event: ', 'dragstart');
+    dragStartIndex = +this.closest('li').getAttribute('data-index');
+}
