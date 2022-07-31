38a803b6f2d246b39f0bbed3506e4a096f73826f
Jeniffer Callou <jenifferprc@gmail.com>
2022-07-31 20:59:51 +0200

    add dragDrop function

--- 19-sortable-list/script.js
+++ 19-sortable-list/script.js
@@ -64,3 +64,11 @@ function dragOver(e) {
     // console.log('Event: ', 'dragover');
     e.preventDefault();
 }
+
+function dragDrop() {
+    // console.log('Event: ', 'drop');
+    const dragEndIndex = +this.getAttribute('data-index');
+    swapItems(dragStartIndex, dragEndIndex);
+  
+    this.classList.remove('over');
+}
