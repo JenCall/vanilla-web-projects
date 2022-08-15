4edaf1fba820ba98437578917966203513214124
Jeniffer Callou <jenifferprc@gmail.com>
2022-08-15 21:30:44 +0200

    start recognition

--- 20-speak-number-game/script.js
+++ 20-speak-number-game/script.js
@@ -1,6 +1,16 @@
 const msgEl = document.getElementById('msg');
 const randomNum = getRandomNumber();
 
+console.log('Number:', randomNum)
+
+window.SpeechRecognition =
+  window.SpeechRecognition || window.webkitSpeechRecognition;
+
+let recognition = new window.SpeechRecognition();
+
+// Start recognition and game
+recognition.start();
+
 // Generate random number
 function getRandomNumber() {
     return Math.floor(Math.random() * 100) + 1;
