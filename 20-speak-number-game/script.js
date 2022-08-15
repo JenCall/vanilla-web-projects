b0fe6bd4dc37944fce623262b15cc6a7d8edadaf
Jeniffer Callou <jenifferprc@gmail.com>
2022-08-15 21:34:56 +0200

    add writeMessage functtion

--- 20-speak-number-game/script.js
+++ 20-speak-number-game/script.js
@@ -19,6 +19,14 @@ function onSpeak(e) {
     checkNumber(msg);
 }
 
+// Write what user speaks
+function writeMessage(msg) {
+    msgEl.innerHTML = `
+      <div>You said: </div>
+      <span class="box">${msg}</span>
+    `;
+}
+
 // Generate random number
 function getRandomNumber() {
     return Math.floor(Math.random() * 100) + 1;
