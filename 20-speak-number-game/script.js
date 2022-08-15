aca3eb065c430e9c76786cfaea293e2b8158a46e
Jeniffer Callou <jenifferprc@gmail.com>
2022-08-15 21:23:37 +0200

    add getRandomNumber function

--- 20-speak-number-game/script.js
+++ 20-speak-number-game/script.js
@@ -1,2 +1,7 @@
 const msgEl = document.getElementById('msg');
 const randomNum = getRandomNumber();
+
+// Generate random number
+function getRandomNumber() {
+    return Math.floor(Math.random() * 100) + 1;
+}
