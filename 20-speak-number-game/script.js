8275140ca66e46dd5946b318a5cb63f2283555ea
Jeniffer Callou <jenifferprc@gmail.com>
2022-08-15 21:32:12 +0200

    add onSpeak functtion

--- 20-speak-number-game/script.js
+++ 20-speak-number-game/script.js
@@ -11,6 +11,14 @@ let recognition = new window.SpeechRecognition();
 // Start recognition and game
 recognition.start();
 
+// Capture user speak
+function onSpeak(e) {
+    const msg = e.results[0][0].transcript;
+  
+    writeMessage(msg);
+    checkNumber(msg);
+}
+
 // Generate random number
 function getRandomNumber() {
     return Math.floor(Math.random() * 100) + 1;
